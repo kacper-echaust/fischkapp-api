01627c687be46b2efc0558a815e717c2ad990897
kacper-echaust <k9echaust@gmail.com>
2024-09-23 19:16:09 +0200

    add timestamps

--- src/models/cardSchema.ts
+++ src/models/cardSchema.ts
@@ -1,26 +1,28 @@
+import { timeStamp } from 'console'
+
 const mongoose = require('mongoose')
 
-const cardSchema = new mongoose.Schema({
-	front: {
-		type: String,
-		required: true,
-		unique: true,
+const cardSchema = new mongoose.Schema(
+	{
+		front: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		back: {
+			type: String,
+			required: true,
+		},
+		tags: {
+			type: [String],
+			default: [],
+		},
+		author: {
+			type: String,
+			required: true,
+		},
 	},
-	back: {
-		type: String,
-		required: true,
-	},
-	tags: {
-		type: [String],
-		default: [],
-	},
-	author: {
-		type: String,
-		required: true,
-	},
-})
+	{ timestamps: true }
+)
 
 export const Card = mongoose.model('Card', cardSchema)
-
-
-
