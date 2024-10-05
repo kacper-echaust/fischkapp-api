44586137d8068f1a227b19937ca4b76144d02992
kacper-echaust <k9echaust@gmail.com>
2024-10-05 10:19:10 +0200

    test finding all cards and by author

---
+++ src/tests/index.test.ts
@@ -0,0 +1,45 @@
+import request from 'supertest'
+import { app } from '../index'
+import { initialCardsMock } from './test-setup'
+
+const AUTHORIZATION_KEY = 'pss-this-is-my-secret'
+
+describe('flashcards', () => {
+	describe('finding cards', () => {
+		describe('get all cards route', () => {
+			it('function need to return code 200,flashcards in the correct order and correct number of flashcards', async () => {
+				const res = await request(app).get('/cards').set('Authorization', AUTHORIZATION_KEY)
+				const data = res.body
+				expect(res.status).toBe(200)
+				expect(data).toBeInstanceOf(Array)
+				expect(data).toEqual(initialCardsMock)
+				expect(data.length).toEqual(initialCardsMock.length)
+
+				const sortedData = [...data].sort((a, b) => {
+					return Date.parse(a.createdAt) - Date.parse(b.createdAt)
+				})
+				expect(initialCardsMock).toEqual(sortedData)
+			})
+		})
+		describe('get cards by author route', () => {
+			it('function returns an array of flashcards written by the requested author in the correct order, status 200', async () => {
+				const filteredAuthor = initialCardsMock.filter(card => {
+					return card.author === 'author1'
+				})
+
+				const res = await request(app).get('/cards/author/author1').set('Authorization', AUTHORIZATION_KEY)
+				const data = res.body
+				console.log(res.body)
+				console.log(filteredAuthor)
+				expect(res.status).toBe(200)
+				expect(data).toBeInstanceOf(Array)
+				expect(data.length).toEqual(filteredAuthor.length)
+
+				const sortedData = [...data].sort((a, b) => {
+					return Date.parse(a.createdAt) - Date.parse(b.createdAt)
+				})
+				expect(initialCardsMock).toEqual(sortedData)
+			})
+		})
+	})
+})
